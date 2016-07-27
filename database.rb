fe9691180e98fb9a6abd3072bd480af4b97d715f
Alan Kebert <adkebert@yahoo.com>
2016-07-27 15:24:45 -0400

    moved person to seperate file and can now add details besides person name

--- database.rb
+++ database.rb
@@ -1,18 +1,33 @@
-class Person
-  attr_accessor :name, :phone_number, :address, :position, :salary, :slack_account, :github_account
+require_relative 'person'
 
-  def initialize(name)
-    self.name = name
+class PersonManager
+
+  def initialize
+    @our_database = []
+  end
+
+  def prompt_for_desired_action
+    puts "What would you like to do with the database? Please enter the corresponding letter"
+    puts "Add a person to the database (A)"
+    puts "Search for a person (S)"
+    puts "Delete a person from the database (D)"
+    gets.chomp.downcase
+  end
+
+  def add_details_to_person(person)
+    politeness = "Please enter #{person.name}'s"
+    puts "#{politeness} phone number"
+    person.phone_number = gets.chomp
+    puts "#{politeness} address"
+    person.address = gets.chomp
+    puts "#{politeness} position"
+    person.position = gets.chomp
+    puts "#{politeness} salary"
+    person.salary = gets.chomp
+    puts "#{politeness} slack account"
+    person.slack_account = gets.chomp
+    puts "#{politeness} github account"
+    person.github_account = gets.chomp
   end
 
 end
-
-def prompt_for_desired_action
-  puts "What would you like to do with the database? Please enter the corresponding letter"
-  puts "Add a person to the database (A)"
-  puts "Search for a person (S)"
-  puts "Delete a person from the database (D)"
-  gets.chomp.downcase
-end
-
-puts prompt_for_desired_action
---
+++ person.rb
@@ -0,0 +1,8 @@
+class Person
+  attr_accessor :name, :phone_number, :address, :position, :salary, :slack_account, :github_account
+
+  def initialize(name)
+    self.name = name
+  end
+
+end
